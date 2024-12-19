b52e4f2bc0c1d20c5b6c957eeea3656a7e092b0c
ViBryanKessel <ViBryanKessel>
2024-12-19 15:54:37 +0000

    Added messages in database to display them

--- app2/views.py
+++ app2/views.py
@@ -1,16 +1,9 @@
 from datetime import datetime
 from django.shortcuts import render
+from app2.models import Message
 
 def index(request):
-    context = {
-        'messages': [{
-        'title': 'My Django App',
-        'content': 'Welcome to my Django app',
-        'created_at': datetime.now()
-    },{
-        'title': 'Another Django',
-        'content': 'Welcome to the other Django app',
-        'created_at': datetime.now()
-    }]
-        }
+    context = {}
+    # The '-' in front of 'created_at' means to order the results in descending order.
+    context['messages'] = Message.objects.order_by('-created_at')
     return render(request, template_name='index.html', context=context)
