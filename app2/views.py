7f896a9038ba35510effe834d0f82a470a6cb008
ViBryanKessel <ViBryanKessel>
2024-12-19 13:41:41 +0000

    Add static file for css, create a view and template

--- app2/views.py
+++ app2/views.py
@@ -7,6 +7,10 @@ def index(request):
         'title': 'My Django App',
         'content': 'Welcome to my Django app',
         'created_at': datetime.now()
+    },{
+        'title': 'Another Django',
+        'content': 'Welcome to the other Django app',
+        'created_at': datetime.now()
     }]
         }
     return render(request, template_name='index.html', context=context)
