77a88b7e68ceb36e4f5e3579e0c692d7f2a036e9
ViBryanKessel <ViBryanKessel>
2024-12-19 16:22:21 +0000

    Create a form for the user to add messages

--- app2/views.py
+++ app2/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 from app2.models import Message
 
 def index(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        user = request.user
+        Message.objects.create(title=title, content=content, user=user)
+        #* OR
+        #* message = Message(title=title, content=content, user=user)
+        #* message.save()
+    
     context = {}
-    # The '-' in front of 'created_at' means to order the results in descending order.
+    #* The '-' in front of 'created_at' means to order the results in descending order.
     context['messages'] = Message.objects.order_by('-created_at')
     return render(request, template_name='index.html', context=context)
